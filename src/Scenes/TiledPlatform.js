f339c19d4841790766b1c7260615063e8ad56f38
Santiago Ponce <saponce@ucsc.edu>
2022-05-20 11:07:58 -0700

    Collection

--- src/Scenes/TiledPlatform.js
+++ src/Scenes/TiledPlatform.js
@@ -108,6 +108,15 @@ class TiledPlatform extends Phaser.Scene {
         // then add the coins to a group
         this.coinGroup = this.add.group(this.coins);
 
+        //Creates a blue powerup for each object named that in the tilemap (since this is an array)
+        this.bluePowerUps = map.createFromObjects("Objects", {
+            name: "bluePowerUp",
+            key: "kenney_sheet",
+            frame: 513
+        });
+        this.physics.world.enable(this.bluePowerUps, Phaser.Physics.Arcade.STATIC_BODY);
+        this.bluePowerUpGroup = this.add.group(this.bluePowerUps);
+
         // set gravity and physics world bounds (so collideWorldBounds works)
         this.physics.world.gravity.y = 2000;
         this.physics.world.bounds.setTo(0, 0, map.widthInPixels, map.heightInPixels);
@@ -117,6 +126,9 @@ class TiledPlatform extends Phaser.Scene {
         this.physics.add.overlap(this.p1, this.coinGroup, (obj1, obj2) => {
             obj2.destroy(); // remove coin on overlap
         });
+        this.physics.add.overlap(this.p1, this.bluePowerUpGroup, (obj1, obj2) => {
+            obj2.destroy(); // remove power up
+        })
 
         // setup camera
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
